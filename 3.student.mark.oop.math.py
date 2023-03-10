a6ec492f9a65282ad26f31437fdbe8cd9fae3f56
NHD-782 <125521102+NHD-782@users.noreply.github.com>
2023-03-10 14:07:07 +0700

    Update 3.student.mark.oop.math.py

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -1,21 +1,25 @@
+# Creat class Student
 class Student:
     def __init__(self, id, name, dob):
         self.id = id
         self.name = name
         self.dob = dob
 
+# Creat class Course
 class Course:
     def __init__(self, id, name, credit):
         self.id = id
         self.name = name
         self.credit = credit
 
+# Creat class Mark
 class Mark:
     def __init__(self, student, course, value):
         self.student = student
         self.course = course
         self.value = value
 
+# Creat class School
 class School:
     def __init__(self):
         self.students = []
@@ -28,6 +32,14 @@ class School:
         dob = input("Student DoB: ")
         student = Student(id, name, dob)
         self.students.append(student)
+        
+    def show_all_student(self):
+        print("Students:")
+        print("| {0: <10} | {1: <20} | {2: <15} |".format('ID', 'Name', 'DoB'))
+        print("-" * 50)
+        for student in self.students:
+            print("| {0: <10} | {1: <20} | {2: <15} |".format(student.id, student.name, student.dob))
+        print()
 
     def add_course(self):
         id = input("Course ID: ")
@@ -35,6 +47,14 @@ class School:
         credit = input("Course credit: ")
         course = Course(id, name, credit)
         self.courses.append(course)
+        
+    def show_all_courses(self):
+        print("Courses:")
+        print("| {0: <10} | {1: <20} | {2: <15} |".format('ID', 'Name', 'Credit'))
+        print("-" * 50)
+        for course in self.courses:
+            print("| {0: <10} | {1: <20} | {2: <15} |".format(course.id, course.name, course.credit))
+        print()
 
     def input_marks(self):
         print("Marks for each student in each course:")
@@ -61,7 +81,7 @@ class School:
             if mark.student == student:
                 course = mark.course
                 credit = int(course.credit)
-                grade_point = self.get_grade_point(mark.value)
+                grade_point = self.grade(mark.value)
                 total_credits += credit
                 total_grade_points += credit * grade_point
         if total_credits > 0:
@@ -70,7 +90,7 @@ class School:
             gpa = 0
         return gpa
 
-    def get_grade_point(self, mark):
+    def grade(self, mark):
         if mark >= 0:
             return mark
 
@@ -79,21 +99,33 @@ class School:
         for student in self.students:
             gpa = self.calculate_gpa(student)
             print(f"{student.name}: {gpa:.2f}")
+            
+    def show_all_mark_GPA(self):
+        print("Marks and GPA for each students:")
+        print("| {0: <20} | {1: <20} | {2: <10} | {3: <5} |".format('Student Name', 'Course Name', 'Value', 'GPA'))
+        print("-" * 70)
+        for mark in self.marks:
+            gpa = self.calculate_gpa(mark.student)
+            print("| {0: <20} | {1: <20} | {2: <10} | {3: <5.2f} |".format(mark.student.name, mark.course.name, mark.value, gpa))
 
 school = School()
 
+# Input the number of students
 n = int(input("Input the number of students:\n"))
 for i in range(n):
     print(f"The information of student {i+1} is: ")
     school.add_student()
+    
+school.show_all_student() # Print all the students input from keyboard
 
+# Input the number of courses
 m = int(input("Enter the number of courses:\n"))
 for i in range(m):
     print(f"Information of course {i+1} is: ")
     school.add_course()
+    
+school.show_all_courses() # Print all the courses input from keyboard
 
-school.input_marks()
+school.input_marks() # Input marks for each student in each course
 
-school.show_gpa()
-
-school.show_marks()
+school.show_all_mark_GPA() # Print all the marks and GPA for each student input from keyboard
